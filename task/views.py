45f7ee4f2db32ae025ee15c4eb960d47bbe490cc
Erik Agayan <erik.agayan1@gmail.com>
2023-07-26 10:40:49 +0300

    Add index html

--- task/views.py
+++ task/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task.models import Tag, Task
+
+
+def index(request):
+    num_tags = Tag.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_newspapers": num_tags,
+        "num_redactors": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task/index.html", context=context)
